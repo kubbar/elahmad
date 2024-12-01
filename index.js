b47cf9caa677472a2f47aee6e9d0159dabbd490d
kubbar <kubbarr@hotmail.com>
2024-12-01 07:34:42 +0000

    ok

--- index.js
+++ index.js
@@ -2,11 +2,25 @@ const express = require('express');
 const puppeteer = require('puppeteer-extra');
 const chromium = require('@sparticuz/chromium');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+const axios = require('axios');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 puppeteer.use(StealthPlugin());
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use('/proxy', createProxyMiddleware({
+  target: '',
+  changeOrigin: true,
+  onProxyReq: (proxyReq, req) => {
+    const targetUrl = decodeURIComponent(req.query.target);
+    proxyReq.setHeader('Referer', 'https://www.elahmad.com');
+    proxyReq.setHeader('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML، مثل Gecko) Chrome/131.0.0.0 Safari/537.36');
+    proxyReq.path = targetUrl;
+  },
+  router: (req) => decodeURIComponent(req.query.target),
+}));
 
 app.get('/:channel', async (req, res) => {
   const { channel } = req.params;
@@ -75,7 +89,10 @@ app.get('/:channel', async (req, res) => {
 
     if (streamingLink) {
       console.log('Streaming Link:', streamingLink);
-      return res.status(200).json({ streamingLink });
+
+      // إنشاء رابط البروكسي
+      const proxyUrl = `${req.protocol}://${req.get('host')}/proxy?target=${encodeURIComponent(streamingLink)}`;
+      return res.status(200).json({ streamingLink: proxyUrl });
     } else {
       console.log('No streaming link found for channel:', channel);
       return res.status(404).json({ error: 'No streaming link found' });
