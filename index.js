4a440754cb23dd1a7c91ee81e9a699cb1a99946a
kubbar <kubbarr@hotmail.com>
2024-12-01 05:08:21 +0000

    ok

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.use('/proxy', createProxyMiddleware({
     proxyReq.setHeader('User-Agent', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML، مثل Gecko) Chrome/131.0.0.0 Safari/537.36');
     proxyReq.path = targetUrl;
   },
+  router: (req) => decodeURIComponent(req.query.target),
 }));
 
 app.get('/:channel', async (req, res) => {
